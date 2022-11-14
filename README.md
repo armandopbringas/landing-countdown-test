3694059686a855bd3d3db0c654d032cbef1e4d42
armanopbringas <bringas.armandop@gmail.com>
2022-11-13 23:10:31 -0600

    fixing hover on embed videos

--- README.md
+++ README.md
@@ -1,70 +1,12 @@
-# Getting Started with Create React App
+Frontend test
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+Requerimientos:
 
-## Available Scripts
+- [x]  El header debe estar fijo en la parte superior
+- [ ]  El contador debe funcionar, teniendo una fecha de término, el día 1 de Diciembre a las 00:00 horas.
+- [x]  Los 4 botones de los videos, deben de cambiar el video superior y activarse, como en el caso del primero. Usa el mismo texto para todos, dentro de los circulos rojos. los videos serian con un embed para tener los 4 videos dentro de la pagina, puedes colcoar los videos que tu quieras. los contenidos y los videos se verían o cambiarian con el hover a cada ciruclo por default se veria el 1 er video con su descripcion
+- [x]  La tipografía se llama Exo
+- [x]  Debe ser responsiva.
+- [x]  El proyecto puedes generarlo con la tecnología que tu gustes
 
-In the project directory, you can run:
-
-### `yarn start`
-
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
-
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
-
-### `yarn test`
-
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `yarn build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `yarn eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
-
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `yarn build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+El código de los videos lo puedes tomar de youtube, los videos que sean de su elección.
--- src/components/video-section/VideoSection.jsx
+++ src/components/video-section/VideoSection.jsx
@@ -1,60 +1,77 @@
-import React from 'react';
-import styles from './VideoSection.module.scss';
-import videoOne from '../../assets/team-latam.png';
-import videoTwo from '../../assets/travel-meets-fashion.png';
-import videoThree from '../../assets/velocidad-riesgo.png';
-import videoFour from '../../assets/experiencia.png';
+import styles from "./VideoSection.module.scss";
 
 const VideoSection = () => {
   return (
-    <div className={styles.videoSectionContainer}>
-      <div>
-        <h2 className={styles.videoSectionTitle}>CLARO SPORT EN SOCHI 2014</h2>
+    <>
+      <h2 className={styles.videoSectionTitle}>CLARO SPORT EN SOCHI 2014</h2>
+      <div className={styles.videoSectionContainer}>
+        <div className={styles.videoCircleContainer}>
+          <figure>
+            <iframe
+              frameBorder="0"
+              title="Inline Frame"
+              className={styles.videoCircle}
+              src="https://www.youtube.com/embed/JWVhVZSdbDE"
+            />
+            <div className={styles.redMask}>
+              <p>
+                Los deportistas Latinoamericanos que participan en Sochi 2014
+              </p>
+            </div>
+          </figure>
+          <p className={styles.text}>TEAM LATAM</p>
+        </div>
+        <div className={styles.videoCircleContainer}>
+          <figure>
+            <iframe
+              frameBorder="0"
+              title="Inline Frame Example"
+              className={styles.videoCircle}
+              src="https://www.youtube.com/embed/JWVhVZSdbDE"
+            />
+            <div className={styles.redMask}>
+              <p>
+                Los deportistas Latinoamericanos que participan en Sochi 2014
+              </p>
+            </div>
+          </figure>
+          <p className={styles.text}>TRAVEL MEETS FASHION</p>
+        </div>
+        <div className={styles.videoCircleContainer}>
+          <figure>
+            <iframe
+              frameBorder="0"
+              title="Inline Frame Example"
+              className={styles.videoCircle}
+              src="https://www.youtube.com/embed/JWVhVZSdbDE"
+            />
+            <div className={styles.redMask}>
+              <p>
+                Los deportistas Latinoamericanos que participan en Sochi 2014
+              </p>
+            </div>
+          </figure>
+          <p className={styles.text}>VELOCIDAD Y RIESGO</p>
+        </div>
+        <div className={styles.videoCircleContainer}>
+          <figure>
+            <iframe
+              frameBorder="0"
+              title="Inline Frame Example"
+              className={styles.videoCircle}
+              src="https://www.youtube.com/embed/JWVhVZSdbDE"
+            />
+            <div className={styles.redMask}>
+              <p>
+                Los deportistas Latinoamericanos que participan en Sochi 2014
+              </p>
+            </div>
+          </figure>
+          <p className={styles.text}>EXPERIENCIA MULTIMEDIA</p>
+        </div>
       </div>
-      <div className={styles.circleVideosContainer}>
-        <div className={styles.videoCircle}>
-          <picture>
-            <img
-              alt='video'
-              src={videoOne}
-              className={styles.videoImgCircle}
-            />
-          </picture>
-          <p>TEAM LATAM</p>
-        </div>
-        <div className={styles.videoCircle}>
-          <picture>
-            <img
-              alt='video'
-              src={videoTwo}
-              className={styles.videoImgCircle}
-            />
-          </picture>
-          <p>TRAVEL MEETS FASSHION</p>
-        </div>
-        <div className={styles.videoCircle}>
-          <picture>
-            <img
-              alt='video'
-              src={videoThree}
-              className={styles.videoImgCircle}
-            />
-          </picture>
-          <p>VELOCIDAD Y RIESGO</p>
-        </div>
-        <div className={styles.videoCircle}>
-          <picture>
-            <img
-              alt='video'
-              src={videoFour}
-              className={styles.videoImgCircle}
-            />
-          </picture>
-          <p>EXPERIENCIA MULIMEDIA</p>
-        </div>        
-      </div>
-    </div>
-  )
-}
+    </>
+  );
+};
 
-export default VideoSection
+export default VideoSection;
